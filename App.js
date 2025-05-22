38a9e34f9b2f2e87e533d2d13ba17054ae3be0d9
demverfaillie@gmail.com <delverfaillie@gmail.com>
2025-05-22 17:11:34 +0200

    commit des ecrans de concert frfr

--- App.js
+++ App.js
@@ -7,12 +7,15 @@ import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
 import MessagesScreen from "./screens/MessagesScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import ConcertScreen from './screens/ConcertScreen.js';
 import { persistStore, persistReducer } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+
 
 import { Provider } from "react-redux";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
@@ -37,20 +40,20 @@ const persistor = persistStore(store);
 const TabNavigator = () => {
   return (
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size, focused }) => {
-          let iconName = '';
-
-          if (route.name === 'Profile') {
-            iconName = 'user-circle';
-          } else if (route.name === 'Messages') {
-            iconName = 'envelope';
-          } else if (route.name === 'Home') {
-            iconName = 'home';
-          }
-
-          return (
-            <View style={[styles.tab, { backgroundColor: focused ? '#E2A5EC' : '#A5ECC0' }]}>
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size, focused }) => {
+        let iconName = '';
+        
+        if (route.name === 'Profile') {
+          iconName = 'user-circle';
+        } else if (route.name === 'Messages') {
+          iconName = 'envelope';
+        } else if (route.name === 'Home') {
+          iconName = 'home';
+        }
+        
+        return (
+          <View style={[styles.tab, { backgroundColor: focused ? '#E2A5EC' : '#A5ECC0' }]}>
               <FontAwesome
                 name={iconName}
                 size={24}
@@ -75,9 +78,12 @@ export default function App() {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <NavigationContainer>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Navigator
+            initialRouteName="Login"
+            screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="TabNavigator" component={TabNavigator} />
+            <Stack.Screen name="ConcertScreen" component={ConcertScreen} />
           </Stack.Navigator>
         </NavigationContainer>
       </PersistGate>
