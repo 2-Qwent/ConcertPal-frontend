d25aed53773e1caf30a5e2f2662761351aa8f9dc
Quentin Rohart <quentin.rohart02@gmail.com>
2025-05-23 10:01:49 +0200

    j'ai oublié ma branche

--- reducers/post.js
+++ reducers/post.js
@@ -8,14 +8,20 @@ export const postSlice = createSlice({
   name: "post",
   initialState,
   reducers: {
+    //ajout d'un post au tableau
     addPost: (state, action) => {
       state.value.push(action.payload);
     },
+    //set le reducer avec tous les posts en BDD
     setPosts: (state, action) => {
-      state.value = action.payload
-    }
+      state.value = action.payload;
+    },
+    //suppression d'un post du tableau
+    deletePost: (state, action) => {
+      state.value = state.value.filter((post) => post._id !== action.payload);
+    },
   },
 });
 
-export const { addPost, setPosts } = postSlice.actions;
+export const { addPost, setPosts, deletePost } = postSlice.actions;
 export default postSlice.reducer;
